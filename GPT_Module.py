c09ab10a9abf1e35eaaad79614a51e0bf45c0867
ralphtawil2 <115980456+ralphtawil2@users.noreply.github.com>
2024-04-04 14:18:42 +0300

    added templates and fixed prompts

--- GPT_Module.py
+++ GPT_Module.py
@@ -33,7 +33,7 @@ class Gpt4:
             "content": [
                 {
                     "type": "text",
-                    "text": f"Given a URL, determine its category from the following options: News, Booking. Analyze the content associated with the URL: {self.url} and select the category that best matches its primary focus or content type. Respond with only the category name and nothing else."
+                    "text": f"Given a URL, determine its category from the following options: News, Booking, E-commerce. Analyze the content associated with the URL: {self.url} and select the category that best matches its primary focus or content type. Respond with only the category name and nothing else."
 
                 }
             ]
@@ -50,24 +50,16 @@ class Gpt4:
             "content": [
                 {
                     "type": "text",
-                    "text": """You're an advanced AI model with specialized OCR and vision capabilities tasked with 
-                    processing split images from a single webpage. Your objective is to identify the website's 
-                    category (news, hotel, e-commerce, or social media) and extract all identifiable text, 
-                    visual elements, and structural layout from these images. In doing so, you must: 
+                    "text": """You are an advanced AI model with specialized OCR and vision capabilities, tasked with processing split images from a single webpage. Your objective is to identify the website's category (news, hotel, e-commerce, or social media) and extract all identifiable text, visual elements, and structural layout from these images. In completing this task, you must:
+                            1- Accurately Piece Together Text: Ensure text that spans across images is correctly pieced together.
+                            
+                            2- Strict Template Adherence: Fill in all fields in the provided JSON template. Important: Do not introduce new fields beyond what the template specifies. If no data is found for a specific field, enter 'null' to maintain template integrity.
 
-                    Accurately piece together text that spans across images. Capture hyperlinks, identifying them 
-                    clearly with placeholder text like "[hyperlink detected]" wherever a hyperlink is present. 
-                    Extract image details, including the image URL (if visible in the image) and any accompanying 
-                    captions or alt text. Identify the headline categories as they appear in the image and sort 
-                    articles under their respective headlines exactly as presented, without adding or assuming 
-                    categories. Structure all extracted data according to the provided JSON template, filling in 
-                    fields such as 'title', 'subtitle', 'images', 'links', and 'categories'. Ensure completeness and 
-                    context integrity, leaving fields null where no data can be extracted. Maintain the hierarchy and 
-                    layout of the webpage content in the JSON structure, keeping the main article and side articles 
-                    distinctly separated and within their actual sections as per the image content. Your analysis 
-                    should accurately reflect the webpage content, capturing all visible details without omission and 
-                    categorizing the content exactly as it is displayed in the images. Adhere strictly to the 
-                    provided JSON template formatting and structure without deviation. """
+                            3- Maintain Content Hierarchy and Layout: Keep the main and side articles distinctly separated according to the webpage's actual sections, ensuring the JSON structure mirrors the webpage layout accurately.
+                            
+                            4- Complete and Accurate Reflection: Your analysis must capture all visible details without omissions, categorizing content exactly as displayed.
+
+                            Adhere strictly to the provided JSON template's formatting and structure, with no deviations. It is crucial that the template is followed precisely, filling available fields with 'null' where applicable and refraining from adding any fields not explicitly listed in the template."""
                 }
             ]
         }]
@@ -104,6 +96,12 @@ class Gpt4:
         response = requests.post("https://api.openai.com/v1/chat/completions", headers=self.headers, json=self.identifier_payload)
         response_json = response.json()
         message_content = response_json['choices'][0]['message']['content']
+        
+        print("Identifier Information")
+        print("The tokens spent on the prompt: "+str(response_json["usage"]["prompt_tokens"]))
+        print("The response tokens : "+str(response_json["usage"]["completion_tokens"]))
+        print("Total tokens spent: "+str(response_json["usage"]["total_tokens"]))
+ 
         return message_content.strip().lower()
         
 
@@ -112,6 +110,12 @@ class Gpt4:
         response = requests.post("https://api.openai.com/v1/chat/completions", headers=self.headers, json=self.extractor_payload)
         response_json = response.json()
         message_content = response_json['choices'][0]['message']['content']
+
+        print("Extractor Information")
+        print("The tokens spent on the prompt: "+str(response_json["usage"]["prompt_tokens"]))
+        print("The response tokens : "+str(response_json["usage"]["completion_tokens"]))
+        print("Total tokens spent: "+str(response_json["usage"]["total_tokens"]))
+
         return message_content
 
     def run(self):
